3d6dcfd57fd161a40f83cbfa88d4722040131156
nodirbekazamov <u900.nodir@gmail.com>
2024-04-30 16:46:58 +0500

    commit5

--- src/components/Buyurtmalarim/Statistika/Statistika.jsx
+++ src/components/Buyurtmalarim/Statistika/Statistika.jsx
@@ -2,6 +2,73 @@ import React from 'react'
 import "./style.scss"
 import Img from "../../../assets/Histogram.png"
 import { SlArrowDown } from "react-icons/sl";
+import { CgCalendarDates } from "react-icons/cg";
+
+import { Table } from 'antd';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Chinese Score',
+    dataIndex: 'chinese',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: 'Math Score',
+    dataIndex: 'math',
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: 'English Score',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
+
 const Statistika = () => {
   return (
     <div className='statistika'>
@@ -77,6 +144,25 @@ const Statistika = () => {
           </div>
         </div>
 
+      </div>
+
+      <div className='statistika_table'>
+        <div className=' table'>
+          <div className='table_date'>
+            <div className='table_inp'>
+              <input type="text" />
+              <p>dan</p>
+            </div>
+            <div className='table_inp'>
+              <input type="text" />
+              <p>gacha</p>
+            </div>
+            <span><CgCalendarDates className='text-[40px]' /></span>
+          </div>
+          <button className='table_btn'>Filter</button>
+        </div>
+
+        <Table columns={columns} dataSource={data} onChange={onChange} />
       </div>
     </div>
   )
